26dcca72a7103c010ea8f2d57ce82c9c64e89574
Nick <nick@odnari.com>
2022-07-12 21:26:33 +0300

    feat: add api service

---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+import fetchRequest, {getUrlWithParams} from './fetchRequest';
+import {endpoints} from '../endpoints';
+
+const defaultFetchMarketsOptions = {
+  vs_currency: 'EUR',
+  per_page: 10,
+};
+
+export const fetchMarkets = (options = {}) => {
+  const url = getUrlWithParams(endpoints.markets, {
+    ...defaultFetchMarketsOptions,
+    ...options,
+  });
+  console.log(url);
+  return fetchRequest(url);
+};
+
+export const fetchCoin = id => {
+  return fetchRequest(endpoints.coinById(id));
+};
