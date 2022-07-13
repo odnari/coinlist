cb467d83bc16ea5ffa4c556c61f79886344781d9
Nick <nick@odnari.com>
2022-07-13 12:46:42 +0300

    feat: add vertical details row and onPress event

--- src/stacks/Markets/Details/components/DetailsRow/DetailsRow.js
+++ src/stacks/Markets/Details/components/DetailsRow/DetailsRow.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from './styles';
 import {Divider, Layout, Text} from '@ui-kitten/components';
+import {Pressable} from 'react-native';
 
-export default function DetailsRow({label, value}) {
+export default function DetailsRow({label, value, vertical, onPress}) {
+  const containerStyle = vertical
+    ? styles.containerVertical
+    : styles.containerHorizontal;
+  const labelStyle = vertical ? styles.labelVertical : {};
+
+  const WrapperComponent = onPress ? Pressable : React.Fragment;
+  const wrapperProps = onPress ? {onPress: () => onPress?.(value)} : {};
+
   return (
-    <React.Fragment>
-      <Layout level="1" style={styles.container}>
-        <Text appearance="hint" category="s1">
+    <WrapperComponent {...wrapperProps}>
+      <Layout level="1" style={containerStyle}>
+        <Text appearance="hint" category="s1" style={labelStyle}>
           {label}
         </Text>
         <Text category="s1">{value}</Text>
       </Layout>
       <Divider />
-    </React.Fragment>
+    </WrapperComponent>
   );
 }
--- src/stacks/Markets/Details/components/DetailsRow/styles.js
+++ src/stacks/Markets/Details/components/DetailsRow/styles.js
@@ -1,11 +1,19 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  containerHorizontal: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     padding: 16,
+  },
+  containerVertical: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+  labelVertical: {
+    marginBottom: 8,
   },
 });
 
