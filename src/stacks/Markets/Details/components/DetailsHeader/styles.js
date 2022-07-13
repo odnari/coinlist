1bf6514ea7109e7b3535de5da850f666577d8bb5
Nick <nick@odnari.com>
2022-07-13 12:46:23 +0300

    feat: make logo bigger

--- src/stacks/Markets/Details/components/DetailsHeader/styles.js
+++ src/stacks/Markets/Details/components/DetailsHeader/styles.js
@@ -2,7 +2,10 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   logo: {
+    aspectRatio: 1.0,
+    height: 96,
     alignSelf: 'center',
+    marginVertical: 8,
   },
 });
 
